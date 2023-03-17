776574f2eb3ebec1d5c9d7fd5aefc4b5bc3b189d
richardbquach <47994922+richardbquach@users.noreply.github.com>
2023-03-17 00:32:34 -0700

    Implemented error handling for cap read

--- src/Camera.py
+++ src/Camera.py
@@ -11,74 +11,78 @@ import time
 import os
 
 if __name__ == "__main__":
-    while true:
+    while True:
     #video_file = r'Video' + os.sep + 'Video_Org.mp4'
     #video = []
-    resolution = (640, 480)
-    camera = PiCamera()
-    camera.resolution = resolution
-    camera.framerate = 30
-    rawCapture = PiRGBArray(camera, size=resolution)
-    time.sleep(0.1)
-    cap = cv.VideoCapture(0)
-    ret, frame = cap.read()
-    # image = frame.array
-    refFrame = frame
-    cv.imshow("Arducam Feed",refFrame)
-    M, N, _ = np.shape(refFrame)
+        resolution = (640, 480)
+        camera = PiCamera()
+        camera.resolution = resolution
+        camera.framerate = 30
+        rawCapture = PiRGBArray(camera, size=resolution)
+        time.sleep(0.1)
+        cap = cv.VideoCapture(0)
 
-    areaThreshold = 200000 #Threshold area of the contour for detecting chess board boundary
-    #Gaussion, canny, dilation and thresholding
-    thresh = m.getBlob(cv.resize(cv.cvtColor(frame, cv.COLOR_BGR2GRAY), (int(N * 0.5), int(M * 0.5))))
-    contours, hierarchy = cv.findContours(thresh, 1, 2)
-    #Finding the boundary
-    boundary = m.getContour(contours, areaThreshold, True)[0]
-    refA = cv.contourArea(boundary)
-    #Approximating the contour to quadilateral
-    boundary = m.getCorners(boundary)
-    intersections, edgePoints = m.getIntersections(boundary)
-
-    # Reading all the frames
-    while ret:
-        video.append(cv.resize(cv.cvtColor(frame, cv.COLOR_BGR2GRAY), (int(N * 0.5), int(M * 0.5))))
         ret, frame = cap.read()
-    cap.release()
-
-    frameCount = len(video)
-    # Finding the stable frames in the video for movement detection
-    stableFrames = m.findStableFrames(video, refA, areaThreshold)
-    print("=============================")
-    print("Stable Frame No:", stableFrames)
-    print("=============================")
-
-    # Creating a chess board object
-    chessboard = ChessBoard()
-
-    i = frameNo = 0
-    # reading the initial chessboard image
-    virtualChessboard = chessboard.getBoardImage()
-    # isFirstFrame = True
-    while True:
-        orgFrame = video[frameNo]
-        if i < len(stableFrames) - 1:
-            if frameNo == stableFrames[i + 1]:
-                # Detecting movement and updating the chess board
-                virtualChessboard = m.detectMovementAndUpdateBoard(video[stableFrames[i]], video[stableFrames[i + 1]],
-                                                                   intersections, chessboard)
-                i = i + 1
-
-        # displaying frames
-        cv.imshow('Original', orgFrame)
-        cv.imshow('Virtual', virtualChessboard)
-
-        cv.waitKey(30)
-        frameNo += 1
-        if not frameNo < frameCount:
+        # image = frame.array
+        refFrame = frame
+        if refFrame is None or refFrame.size == 0:
+            print("HELP")
             break
+        else:
+        #cv.imshow("Arducam Feed",refFrame)
+            M, N, _ = np.shape(refFrame)
 
-        # if isFirstFrame:
-        #     cv.waitKey(30000*2)
-        #     isFirstFrame = False
+        areaThreshold = 200000 #Threshold area of the contour for detecting chess board boundary
+        #Gaussion, canny, dilation and thresholding
+        thresh = m.getBlob(cv.resize(cv.cvtColor(frame, cv.COLOR_BGR2GRAY), (int(N * 0.5), int(M * 0.5))))
+        contours, hierarchy = cv.findContours(thresh, 1, 2)
+        #Finding the boundary
+        boundary = m.getContour(contours, areaThreshold, True)[0]
+        refA = cv.contourArea(boundary)
+        #Approximating the contour to quadilateral
+        boundary = m.getCorners(boundary)
+        intersections, edgePoints = m.getIntersections(boundary)
 
+        # Reading all the frames
+        while ret:
+            video.append(cv.resize(cv.cvtColor(frame, cv.COLOR_BGR2GRAY), (int(N * 0.5), int(M * 0.5))))
+            ret, frame = cap.read()
+        cap.release()
+
+        frameCount = len(video)
+        # Finding the stable frames in the video for movement detection
+        stableFrames = m.findStableFrames(video, refA, areaThreshold)
+        print("=============================")
+        print("Stable Frame No:", stableFrames)
+        print("=============================")
+
+        # Creating a chess board object
+        chessboard = ChessBoard()
+
+        i = frameNo = 0
+        # reading the initial chessboard image
+        virtualChessboard = chessboard.getBoardImage()
+        # isFirstFrame = True
+        while True:
+            orgFrame = video[frameNo]
+            if i < len(stableFrames) - 1:
+                if frameNo == stableFrames[i + 1]:
+                    # Detecting movement and updating the chess board
+                    virtualChessboard = m.detectMovementAndUpdateBoard(video[stableFrames[i]], video[stableFrames[i + 1]],
+                                                                       intersections, chessboard)
+                    i = i + 1
+
+            # displaying frames
+            cv.imshow('Original', orgFrame)
+            cv.imshow('Virtual', virtualChessboard)
+
+            cv.waitKey(30)
+            frameNo += 1
+            if not frameNo < frameCount:
+                break
+
+            # if isFirstFrame:
+            #     cv.waitKey(30000*2)
+            #     isFirstFrame = False
 
 
